84def2de2abe084fd93a9d16ceb0cfb364932b77
ramii1 <ramyar6@hotmail.com>
2017-03-29 09:22:23 +0200

    change CmakeList and video

--- video.cpp
+++ video.cpp
@@ -1,36 +1,49 @@
-#include <iostream>
-#include <opencv2/highgui.hpp>
-#include <opencv2/imgproc.hpp>
+#include <ros/ros.h>
+#include <std_msgs/Empty.h>
+#include <image_transport/image_transport.h>
+#include <cv_bridge/cv_bridge.h>
+#include <sensor_msgs/image_encodings.h>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/objdetect/objdetect.hpp>
 
 using namespace std;
 using namespace cv;
 
+
+#include <cstdlib>
+
+using namespace ros;
+using namespace std;
+using namespace cv;
+
+static const char WINDOW[]="RGB Image";
+static const char WINDOW2[]="Gray Image";
+static const std::string OPENCV_WINDOW = "Image window";
+static int drone_state;
+
+std_msgs::Empty emp_msg;
+
+void process(const sensor_msgs::ImageConstPtr& msg)
+     {
+       cv_bridge::CvImagePtr cv_ptr;
+       try
+       {
+         cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
+       }
+       catch (cv_bridge::Exception& e)
+       {
+         ROS_ERROR("cv_bridge exception: %s", e.what());
+         return;
+       }
+
 int main()
 {
-    cv::VideoCapture cap;
-    cv::Mat image;
+      image_transport::ImageTransport it(n);
+  image_transport::Subscriber image_sub = it.subscribe("/ardrone/image_raw",1,process);
 
-    if (!cap.open("tcp://192.168.1.1:5555"))
-    {
-        printf("AR.Drone ERROR CONNECT\n");
-        return -1;
-    }
-
-    while (1)
-    {
-        cap >> image;
-
-        if (!image.empty())
-        {
-            cv::imshow("AR.Drone", image);
-            cout << "OK" << endl;
-        }
-        else
-        {
-            cout << "ERROR" << endl;
-            cv::waitKey(1);
-        }
-    }
+  cv::namedWindow(WINDOW);
+  cv::namedWindow(WINDOW2);
 
     return 0;
 }
